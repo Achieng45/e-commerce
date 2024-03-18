0440eadf750387d5f0f33dbc9522b09c425c5caa
Achieng45 <stephanieachieng45@gmail.com>
2024-03-18 14:20:17 +0300

    added two navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'topnav',component:TopNavbarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
