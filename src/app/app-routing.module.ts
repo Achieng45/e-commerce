06ed4df2bda0ce611d7cd49be4393e19afe457c5
Achieng45 <stephanieachieng45@gmail.com>
2024-03-19 11:16:01 +0300

    side nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 const routes: Routes = [
-  {path:'topnav',component:TopNavbarComponent}
+  {path:'topnav',component:TopNavbarComponent},
+  {path:'sidenav',component:SideNavComponent}
 ];
 
 @NgModule({
