400ec7ce31f7d57c6b9730eb8f6200c1bf9ee1ca
Achieng45 <stephanieachieng45@gmail.com>
2024-03-19 12:22:48 +0300

    trending div

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesTrendingComponent } from './categories-trending/categories-trending.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'trending',component:CategoriesTrendingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
