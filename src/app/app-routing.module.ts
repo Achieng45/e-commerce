eafb91954a84cb592a8ef18d23aa9dbfa87a1d19
Achieng45 <stephanieachieng45@gmail.com>
2024-03-20 13:57:18 +0300

    our-services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriesTrendingComponent } from './categories-trending/categories-trending.component';
+import { OurServicesComponent } from './our-services/our-services.component';
 
 const routes: Routes = [
-  {path:'trending',component:CategoriesTrendingComponent}
+  {path:'trending',component:CategoriesTrendingComponent},
+  {path:'services',component:OurServicesComponent}
 ];
 
 @NgModule({
